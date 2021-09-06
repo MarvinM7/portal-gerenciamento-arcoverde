414afea536b912fd8529760d05d4c31a69e46c2c
Vitor Sousa <32624576+vss-2@users.noreply.github.com>
2021-09-06 19:27:30 -0300

    Implementacao de secao de ocorrencias na tela de EmployeeForm

--- src/components/EmployeeFormOcurrences/EmployeeFormOcurrences.jsx
+++ src/components/EmployeeFormOcurrences/EmployeeFormOcurrences.jsx
@@ -1,11 +1,21 @@
+import { Button } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 import ColumnItem from "../../components/ColumnItem/ColumnItem";
+import { TextField } from '@material-ui/core';
+import SaveIcon from '@material-ui/icons/Save';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Container from 'react-bootstrap/Container';
+import ButtonStatus from './ButtonStatus';
 
 const EmployeeFormOcurrences = () => {
+  const [ocurrence, setOcurrence] = useState('');
+
   const data = [
     [
       {
@@ -81,6 +91,52 @@ const EmployeeFormOcurrences = () => {
             })}
           </Row>
         </Col>
+          <Container fluid>
+            <div className="simple-space"></div>
+            <div className="simple-space"></div>
+            <Row>
+              <Col xs={10}>
+                  <TextField
+                    fullWidth={true}
+                    variant='outlined'
+                    id='new-ocurrence'
+                    label='Nova ocorrência'
+                    defaultValue={ocurrence}
+                    onChange={(e) => setOcurrence(e.target.value)}
+                  />
+              </Col>
+              <Col>
+                  <Box border="2px solid #06933C" padding="8px 8px" marginRight="20px">
+                    {props => 
+                      <Button {...props} variant="outlined" color="primary">
+                        <SaveIcon  htmlColor={ButtonStatus('enabled')}/>
+                      </Button> 
+                    }
+                  </Box>
+                  <Box border="2px solid #06933C" marginRight="20px">
+                    {props => 
+                      <Button {...props} variant="outlined" color="primary">
+                        <EditIcon htmlColor={ButtonStatus('disabled')}/>
+                      </Button> 
+                    }
+                  </Box>
+                  <Box border="2px solid #06933C"> 
+                    {props => 
+                      <Button {...props} variant="outlined" color="primary">
+                        <DeleteIcon htmlColor={ButtonStatus('disabled')}/>
+                      </Button> 
+                    }
+                  </Box>
+              </Col>
+              <div className="simple-space"></div>
+              <div className="simple-space"></div>
+              <Row className='justify-content-evenly'>
+                <Col className='text-center' xs='10' sm='4' md='3' lg='3' xl='1'>
+                  <Button variant="contained" color="primary" disableElevation>Imprimir</Button>
+                </Col>
+              </Row>
+            </Row>
+          </Container>
       </Row>
     </React.Fragment>
   )
